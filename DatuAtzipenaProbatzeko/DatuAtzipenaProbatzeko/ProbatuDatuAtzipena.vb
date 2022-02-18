8bd20d23e222bfe021005a3fe9f5ec0edbf7238e
bsarasuspe <83415989+bsarasuspe@users.noreply.github.com>
2022-02-18 17:50:32 +0100

    Commit

---
+++ DatuAtzipenaProbatzeko/DatuAtzipenaProbatzeko/ProbatuDatuAtzipena.vb
@@ -0,0 +1,25 @@
+﻿Module ProbatuDatuAtzipena
+
+    Sub Main()
+
+        Console.WriteLine("Konexioa egin:")
+        DatuAtzipenekoak.DatuAtzipena.Konektatu()
+
+        Console.WriteLine("Erabiltzailea sartu:") 'KOMENTATUTA DAGO DAGOENEKO TXERTATUTA DAGOELAKO
+        Console.WriteLine(DatuAtzipenekoak.DatuAtzipena.ErabiltzaileaTxertatu("popeye2@gmail.com", "Popeye2", "None", "Kaixo", "Erantzuna", "72593749", "123", "0", "A", "B", "Erab", "Pass"))
+
+        Console.WriteLine("Erabiltzailea lortu:")
+        Console.WriteLine(DatuAtzipenekoak.DatuAtzipena.ErabiltzaileaLortu("popeye@gmail.com"))
+
+        Console.WriteLine("Erabiltzailea egiaztatu:")
+        Console.WriteLine(DatuAtzipenekoak.DatuAtzipena.ErabiltzaileaEgiaztatu("popeye@gmail.com"))
+
+        Console.WriteLine("Erabiltzailearen pasahitza aldatu:")
+        Console.WriteLine(DatuAtzipenekoak.DatuAtzipena.ErabiltzailearenPasahitzaAldatu("popeye@gmail.com", "Pass2"))
+
+        Console.WriteLine("Konexioa itxi:")
+        DatuAtzipenekoak.DatuAtzipena.ItxiKonexioa()
+
+    End Sub
+
+End Module
